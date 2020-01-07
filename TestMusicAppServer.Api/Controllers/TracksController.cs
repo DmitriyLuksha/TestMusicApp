54c3d66c3c1178d7460ed539bd9f37b152a28494
nanpx <nanpxmail@gmail.com>
2020-01-07 16:13:59 +0300

    Update size limit to Windows MB

--- TestMusicAppServer.Api/Controllers/TracksController.cs
+++ TestMusicAppServer.Api/Controllers/TracksController.cs
@@ -24,7 +24,7 @@ namespace TestMusicAppServer.Api.Controllers
 
         [HttpPost]
         [Route("uploadFile")]
-        [RequestSizeLimit(30 * 1024 * 1024)]
+        [RequestSizeLimit(30 * 1000 * 1000)]
         public async Task<IActionResult> UploadFile([FromForm] UploadFileCommand command)
         {
             command.UserId = _accountService.UserId;
