31e1ba3daf9decf8d039d835d350e5ef890bf9c8
nanpx <nanpxmail@gmail.com>
2020-01-18 17:49:46 +0300

    Remove obsolete UseApplicationInsights call

--- TestMusicAppServer.Api/Program.cs
+++ TestMusicAppServer.Api/Program.cs
@@ -12,7 +12,6 @@ namespace TestMusicAppServer.Api
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseApplicationInsights()
                 .UseStartup<Startup>();
     }
 }
